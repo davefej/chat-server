34b58156d9b6926d213851513915391f6af9c5f5
davefej <davefej@gmail.com>
2018-06-05 17:02:20 +0200

    update

--- public/static/swextra.js
+++ public/static/swextra.js
@@ -22,39 +22,42 @@ function syncMessages(){
 
 function syncMessage(msg){
 
+  let dbPromise = null;
+  const openRequest = indexedDB.open( 'db', 1 );
+  openRequest.onsuccess = function(e) {
+    dbPromise = e.target.result;
+    dbPromise.then(function(db) {
+      var tx = db.transaction('store', 'readonly');
+      var store = tx.objectStore('store');
+      return store.get('sandwich');
+    }).then(function(msg) {
 
-  const dbPromise = indexedDB.open( 'db', 1 );
-  dbPromise.then(function(db) {
-    var tx = db.transaction('store', 'readonly');
-    var store = tx.objectStore('store');
-    return store.get('sandwich');
-  }).then(function(msg) {
-
-    setTimeout(function(){
-      fetch("messages/" + msg.msgId, {
-        mode:"no-cors",
-        method: 'post',
-        headers: {
-          'Content-type': 'application/json'
-        },
-        body: JSON.stringify({
-          msg: {
-            txt: msg.txt,
-            sender: msg.sender
-          }
-        }),
-      }).then(function (response) {
-        return response;
-      })
-        .then(function (text) {
-          console.log('Request successful', text);
+      setTimeout(function(){
+        fetch("messages/" + msg.msgId, {
+          mode:"no-cors",
+          method: 'post',
+          headers: {
+            'Content-type': 'application/json'
+          },
+          body: JSON.stringify({
+            msg: {
+              txt: msg.txt,
+              sender: msg.sender
+            }
+          }),
+        }).then(function (response) {
+          return response;
         })
-        .catch(function (error) {
-          console.log('Request failed', error);
-        });
-    },1000);
+          .then(function (text) {
+            console.log('Request successful', text);
+          })
+          .catch(function (error) {
+            console.log('Request failed', error);
+          });
+      },1000);
 
-  });
+    });
+  };
 /*
   setTimeout(function(){
     fetch("messages/" + msg.msgId, {
