633839f73678a6cd7cf70c3edabbb1b3b9004206
davefej <davefej@gmail.com>
2018-06-06 10:54:53 +0200

    update

--- public/static/swextra.js
+++ public/static/swextra.js
@@ -19,73 +19,60 @@ function syncMessages(){
   }
   unsentmessages = [];
 }
+var DBVERSION = 8;
 
-function syncMessage(msg){
+function syncMessage(msg) {
 
-  let dbPromise = null;
-  const openRequest = indexedDB.open( 'db', 4 );
-  openRequest.onsuccess = function(e) {
+  var request = indexedDB.open("db", DBVERSION);
+  request.onsuccess = function (event) {
+    var db = event.target.result;
+    var tx = db.transaction("unSyncedMessages", 'readwrite');
+    var store = tx.objectStore("unSyncedMessages");
+    var items = store.getAll().onsuccess = function (event) {
+      var dataarr = event.target.result;
+      setTimeout(function () {
 
-      var db = event.target.result;
-      var tx = db.transaction('unSyncedMessages', 'readwrite');
-      var store = tx.objectStore('unSyncedMessages');
-      var arr = store.getAll();
-      for(var i = 0; i < arr.length;i++){
-        var msg = arr[i];
-        setTimeout(function(){
+        for (var i = 0; i < dataarr.length; i++) {
+          var msg = dataarr[i];
           fetch("messages/" + msg.msgId, {
-            mode:"no-cors",
+            mode: "cors",
             method: 'post',
             headers: {
-              'Content-type': 'application/json'
+              'Content-type': 'application/json; charset=utf-8'
             },
             body: JSON.stringify({
               msg: {
                 txt: msg.txt,
                 sender: msg.sender
               }
-            }),
+            })
           }).then(function (response) {
             return response;
-          })
-            .then(function (text) {
-              console.log('Request successful', text);
-            })
-            .catch(function (error) {
-              console.log('Request failed', error);
-            });
-        },1000);
-      }
-
-
-
-  };
-/*
-  setTimeout(function(){
-    fetch("messages/" + msg.msgId, {
-      mode:"no-cors",
-      method: 'post',
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify({
-        msg: {
-          txt: msg.txt,
-          sender: msg.sender
+          }).then(function (text) {
+            console.log('Request successful', text);
+          }).catch(function (error) {
+            console.log('Request failed', error);
+          });
         }
-      }),
-    }).then(function (response) {
-      return response;
-    })
-      .then(function (text) {
-        console.log('Request successful', text);
-      })
-      .catch(function (error) {
-        console.log('Request failed', error);
-      });
-  },1000);
-*/
+        var request = indexedDB.open("db", DBVERSION);
+        request.onsuccess = function (event) {
+          var db = event.target.result;
+          var tx = db.transaction("unSyncedMessages", 'readwrite');
+          var store = tx.objectStore("unSyncedMessages");
+          var objectStoreRequest = store.clear();
+          objectStoreRequest.onsuccess = function (event) {
+            console.log("Cleared!");
+          };
+        }
+
+
+      }, 500);
+
+
+    }
+
+  }
+  request.onerror = function (err) {
+    console.err(err);
+  }
 }
-
-
-
